7da72e2de9856701d293184201b4ea21387dc802
webdev-A <99373313+webdev-A@users.noreply.github.com>
2022-02-10 10:01:10 +0800

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,63 @@
+(function($) {
+    "use strict";
+	
+	/* ..............................................
+	Loader 
+    ................................................. */
+	
+	$(window).on('load', function() { 
+		$('.preloader').fadeOut(); 
+		$('#preloader').delay(550).fadeOut('slow'); 
+		$('body').delay(450).css({'overflow':'visible'});
+	});
+	
+	
+	/* ..............................................
+    Gallery
+    ................................................. */
+	
+	$('#slides').superslides({
+		inherit_width_from: '.cover-slides',
+		inherit_height_from: '.cover-slides',
+		play: 5000,
+		animation: 'fade',
+	});
+	
+	$( ".cover-slides ul li" ).append( "<div class='overlay-background'></div>" );
+	
+	/* ..............................................
+    Map Full
+    ................................................. */
+	
+	$(document).ready(function(){ 
+		$(window).on('scroll', function () {
+			if ($(this).scrollTop() > 100) { 
+				$('#back-to-top').fadeIn(); 
+			} else { 
+				$('#back-to-top').fadeOut(); 
+			} 
+		}); 
+		$('#back-to-top').click(function(){ 
+			$("html, body").animate({ scrollTop: 0 }, 600); 
+			return false; 
+		}); 
+	});
+	
+	
+	
+	/* ..............................................
+    BaguetteBox
+    ................................................. */
+	
+	baguetteBox.run('.tz-gallery', {
+		animation: 'fadeIn',
+		noScrollbars: true
+	});
+	
+	
+	
+	
+	
+	
+	
+}(jQuery));
